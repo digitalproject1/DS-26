8f4db5588c1a312d0e21953feb515ca68fc93ecc
Don Juan <139347029+digitalproject1@users.noreply.github.com>
2023-07-26 18:06:07 +0700

    ds_dataprep_clean_app.py

--- ds_dataprep_clean_app.py
+++ ds_dataprep_clean_app.py
@@ -37,7 +37,7 @@ def load_data(data):
 
 def run_ds_dataprep_clean_app():
     st.write(temp,unsafe_allow_html=True)
-    df = load_data("/Data Privasi/Modul Programming/Python/survey.csv")
+    df = load_data("/survey.csv")
     
     # Data Cleansing for Null Value and Unnecessary Features
     columns_to_drop = ['state', 'comments', 'Timestamp']
