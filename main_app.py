7d957246ee2e2f6fd8b7c89a7eea20ecf4840351
Don Juan <139347029+digitalproject1@users.noreply.github.com>
2023-07-27 12:22:37 +0700

    main_app.py

--- main_app.py
+++ main_app.py
@@ -119,10 +119,10 @@ def main_section():
      
      col1, mid, col2 = st.columns([1,1,25])
      with col1:
-            st.image('main/unicorn.png', width=60)
+            st.image('/Data Privasi/archive/unicorn.png', width=60)
      with col2:
             st.write('Unicorn Team')
-            st.image("main/DS.png", caption="Data Science Batch 26", use_column_width=True)
+            st.image("/Data Privasi/Modul Programming/Python/Final Project/image/DS.png", caption="Data Science Batch 26", use_column_width=True)
 
      st.write(home_temp,unsafe_allow_html=True)
      st.write(home2_temp,unsafe_allow_html=True)
@@ -134,10 +134,10 @@ def main_section():
      col1, col2, col3, col4 = st.columns(4)
 
      team_members = {
-          "Anindya Lokeswara": "main/Anin.jpg",
-          "Tatag Suryo Pambudi": "main/Tatag.jpg",
-          "Erdiah Ashida Nasirin": "main/Erdiah.jpg",
-          "Andi Muhammad Yusuf": "main/Andi.jpg"
+          "Anindya Lokeswara": "/Data Privasi/Modul Programming/Python/Final Project/image/Anin.jpg",
+          "Tatag Suryo Pambudi": "/Data Privasi/Modul Programming/python/Final Project/image/Tatag.jpg",
+          "Erdiah Ashida Nasirin": "/Data Privasi/Modul Programming/Python/Final Project/image/Erdiah.jpg",
+          "Andi Muhammad Yusuf": "/Data Privasi/Modul Programming/Python/Final Project/image/Andi.jpg"
      }
 
      # Use the columns to align images side by side
